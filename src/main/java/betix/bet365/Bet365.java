16bbeba5173d6d2c4efa3c87ccf14b3f653e5ecc
stitch <macypa@gmail.com>
2015-02-08 12:03:24 +0200

    collect info implementation part

--- src/main/java/betix/bet365/Bet365.java
+++ src/main/java/betix/bet365/Bet365.java
@@ -1,18 +1,18 @@
 package betix.bet365;
 
 
+import betix.core.AccountInfo;
 import betix.core.ConfigKey;
 import betix.core.Configuration;
 import org.sikuli.basics.SikuliScript;
-import org.sikuli.script.App;
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
+import org.sikuli.script.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 
 /**
  * login page https://members.788-sb.com/MEMBERS/Login/
@@ -34,11 +34,19 @@ public class Bet365 {
     private static final Configuration config = new Configuration();
 
     static Screen screen = new Screen();
+    static AccountInfo accountInfo = new AccountInfo();
     private static final String DIR_PATTERN = config.getConfigAsString(ConfigKey.imageDir) + File.separator + config.getConfigAsString(ConfigKey.siteName) + File.separator;
     private static final Pattern PATTERN_LOGO = new Pattern(DIR_PATTERN + "logo.png");
+    private static final Pattern PATTERN_HISTORY_BALANCE = new Pattern(DIR_PATTERN + "historyBalance.png");
     private static final Pattern PATTERN_HISTORY_LINK = new Pattern(DIR_PATTERN + "historyLink.png");
     private static final Pattern PATTERN_HISTORY_DATE = new Pattern(DIR_PATTERN + "historyDate.png");
     private static final Pattern PATTERN_RADIO_BUTTON = new Pattern(DIR_PATTERN + "radioButton.png");
+    private static final Pattern PATTERN_HISTORY_MATCH_LINK = new Pattern(DIR_PATTERN + "historyMatchLink.png");
+    private static final Pattern PATTERN_HISTORY_PENDING_DROPDOWN = new Pattern(DIR_PATTERN + "historyPendingDropdown.png");
+    private static final Pattern PATTERN_HISTORY_PENDING = new Pattern(DIR_PATTERN + "historyPending.png");
+    private static final Pattern PATTERN_HISTORY_FINISHED_DROPDOWN = new Pattern(DIR_PATTERN + "historyFinishedDropdown.png");
+    private static final Pattern PATTERN_HISTORY_FINISHED = new Pattern(DIR_PATTERN + "historyFinished.png");
+    private static final Pattern PATTERN_HISTORY_SELLECTION_TYPE_DRAW = new Pattern(DIR_PATTERN + "historySellectionTypeDraw.png");
     private static final Pattern PATTERN_HISTORY_BUTTON_FIND = new Pattern(DIR_PATTERN + "historyButtonFind.png");
     private static final Pattern PATTERN_VIEW_PAGE_SOURCE = new Pattern(DIR_PATTERN + "viewPageSource.png");
     private static final Pattern PATTERN_FOOTBALL_LINK = new Pattern(DIR_PATTERN + "football.png").similar(0.5f);
@@ -54,9 +62,9 @@ public class Bet365 {
             System.exit(1);
         }
 
-        cloectInfo();
+        collectInfo();
 
-        openFootbalPage();
+        //openFootbalPage();
     }
 
     private static void openSite() {
@@ -138,25 +146,22 @@ public class Bet365 {
         return false;
     }
 
-    private static void cloectInfo() {
+    private static void collectInfo() {
 
         try {
+
             screen.click(PATTERN_HISTORY_LINK);
             wait(1);
+
+            getBalanceInfo();
+
             screen.find(PATTERN_HISTORY_DATE).click(PATTERN_RADIO_BUTTON);
             wait(1);
             screen.click(PATTERN_HISTORY_BUTTON_FIND);
             wait(1);
-//            screen.rightClick();
-//            wait(1);
-//            screen.click(PATTERN_VIEW_PAGE_SOURCE);
-//            wait(1);
-//            screen.type("a", KeyModifier.CTRL);
-//            wait(1);
-//            screen.type("c", KeyModifier.CTRL);
-//            wait(1);
-//            String source = Env.getClipboard();
-//            System.out.println("source = " + source);
+
+            collectPendingMatchesInfo();
+            collectFinishedMatchesInfo();
 
             // click on all "Равен @" img in loop
             // search for  "Равен" img as black text
@@ -182,8 +187,125 @@ public class Bet365 {
 
                      */
 
+//            screen.rightClick();
+//            wait(1);
+//            screen.click(PATTERN_VIEW_PAGE_SOURCE);
+//            wait(1);
+//            screen.type("a", KeyModifier.CTRL);
+//            wait(1);
+//            screen.type("c", KeyModifier.CTRL);
+//            wait(1);
+//            String source = Env.getClipboard();
+//            System.out.println("source = " + source);
         } catch (FindFailed e) {
             e.printStackTrace();
+        } finally {
+            screen.type(Key.F4, KeyModifier.CTRL);
+        }
+    }
+
+    private static void getBalanceInfo() {
+
+        // double click on Баланс img
+            /*
+            Спорт История (BGN)
+                    198,55 BGN
+                    Баланс
+            */
+
+        try {
+            screen.doubleClick(PATTERN_HISTORY_BALANCE);
+            screen.type(Key.UP, KeyModifier.SHIFT);
+            wait(1);
+            screen.type("c", KeyModifier.CTRL);
+            wait(1);
+            String balanceInfo = Env.getClipboard();
+            logger.info("found balanceInfo = " + balanceInfo);
+
+            java.util.regex.Pattern MY_PATTERN = java.util.regex.Pattern.compile(".*?\\(BGN\\)\\s*(.*?)\\s*BGN");
+            Matcher m = MY_PATTERN.matcher(balanceInfo);
+            while (m.find()) {
+                String s = m.group(1);
+                Double balance = Double.valueOf(s.replaceAll(",", "."));
+                logger.info("found balance = " + balance);
+                accountInfo.setBalance(balance);
+            }
+        } catch (FindFailed findFailed) {
+            logger.error("can't get balance .... no biggy.");
+        }
+    }
+
+    /**
+     * // click on all "Равен @" img in loop
+     * // search for  "Равен" img as black text
+     * // type shift+Dwon
+     * // copy clipborad
+     * // parse text
+     * // save to config
+     * <p>
+     * <p>
+     * Равен 	Фулъм v Съндърланд
+     * (Краен Резултат) 	03/02/2015 	Никой 	3.40 	Загубен
+     * Залог:  0,50   Печалби:  0,00
+     * <p>
+     * <p>
+     * <p>
+     * Равен 	Клермон Фуут v Ниор
+     * (Краен Резултат) 	06/02/2015 	Никой 	3.10 	Печеливш
+     * Залог:  0,50   Печалби:  1,55
+     *
+     * @throws FindFailed
+     */
+    private static void collectPendingMatchesInfo() throws FindFailed {
+        Iterator<Match> matches = screen.findAll(PATTERN_HISTORY_MATCH_LINK);
+        while (matches.hasNext()) {
+            Match match = matches.next();
+            match.click();
+            wait(1);
+
+            screen.doubleClick(PATTERN_HISTORY_SELLECTION_TYPE_DRAW);
+
+            screen.type(Key.DOWN, KeyModifier.SHIFT);
+            wait(1);
+            screen.type("c", KeyModifier.CTRL);
+            wait(1);
+            String matchInfo = Env.getClipboard();
+            logger.info("found matchInfo = " + matchInfo);
+
+            //colapse match info
+            match.click();
+            wait(1);
+        }
+    }
+
+    private static void collectFinishedMatchesInfo() throws FindFailed {
+        Match pendingDropdown = screen.find(PATTERN_HISTORY_PENDING_DROPDOWN);
+        pendingDropdown.click();
+        wait(1);
+        pendingDropdown.click();
+        wait(1);
+
+        screen.click(PATTERN_HISTORY_FINISHED);
+        wait(1);
+
+        Iterator<Match> matches = screen.findAll(PATTERN_HISTORY_MATCH_LINK);
+        while (matches.hasNext()) {
+            Match match = matches.next();
+            match.click();
+            wait(1);
+
+            screen.doubleClick(PATTERN_HISTORY_SELLECTION_TYPE_DRAW);
+
+            screen.type(Key.DOWN, KeyModifier.SHIFT);
+            wait(1);
+            screen.type("c", KeyModifier.CTRL);
+            wait(1);
+            String matchInfo = Env.getClipboard();
+            logger.info("found matchInfo = " + matchInfo);
+
+            //colapse match info
+            match.click();
+            wait(1);
         }
     }
 
---
+++ src/main/java/betix/core/AccountInfo.java
@@ -0,0 +1,14 @@
+package betix.core;
+
+public class AccountInfo {
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
